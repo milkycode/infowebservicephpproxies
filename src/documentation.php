112b4b49106fc0af3c0f9707d6358acf326798ab
Thomas Langer <tlanger@edvlanger.de>
2014-04-23 01:59:23 +0200

    Mainpage hinzugefügt.

---
+++ src/documentation.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * \mainpage Webservice Proxy Documentation
+ * 
+ * Use the CustomerInfoWebserviceProxy to get access to the Promotional Office Info Webservice
+ * 
+ * \page page1 Testpage
+ */
+
+?>
