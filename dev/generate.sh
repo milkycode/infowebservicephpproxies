37a1fc20353de6b3139e3b1f8328a21f0468f109
Matthias Kleine <matthias@klein0r.de>
2015-02-03 10:50:24 +0100

    Service class generation script (generate.sh)

---
+++ dev/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "Please specify hostname"
+else
+    echo "CustomerImportService..."
+    rm -rf ./CustomerImportService/
+    php wsdl2phpgenerator-2.5.4.phar -i http://$1/PromotionalOffice/Services/UniversalImporter/CustomerImportService.svc?wsdl -o ./CustomerImportService/
+
+    echo "ClientBackendService..."
+    rm -rf ./ClientBackendService/
+    php wsdl2phpgenerator-2.5.4.phar -i https://$1/PromotionalOffice/Services/Info/ClientBackendService.svc?wsdl -o ./ClientBackendService/
+
+    echo "UniversalImporterDocumentationService..."
+    rm -rf ./UniversalImporterDocumentationService/
+    php wsdl2phpgenerator-2.5.4.phar -i http://$1/PromotionalOffice/Services/UniversalImporter/UniversalImporterDocumentationService.svc?wsdl -o ./UniversalImporterDocumentationService/
+fi
