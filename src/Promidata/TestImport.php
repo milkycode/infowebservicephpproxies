7e0616a1ab8a5d36c936ebbc8e6929c425506594
chris <christian@promidata.com>
2015-04-20 14:57:06 +0200

    - weg

--- src/Promidata/TestImport.php
+++ src/Promidata/TestImport.php
@@ -6,22 +6,21 @@ ini_set('display_errors', 1);
 require_once(dirname(__FILE__) . '/init.php');
 
 try {
-	//
     $proxy = new Promidata_Service_Proxy_CustomerImportService('promotionaloffice.cloudapp.net/PromotionalOffice/Services/UniversalImporter/CustomerImportService.svc');
 
 	$proxy->Logon('CompanyName', 'UserName', 'Password');
-			
+
 	// Example with Exists Method
 	$dataSelector = new Promidata_Service_DTO_DataSelector("ExternReferenceSelector");
 	$dataSelector->add("ImportSource", "MeineImportSource");
 	$dataSelector->add("ExternId", "MeineExternId");
-	
+
 	$existsArgument = new Promidata_Service_DTO_ExistsArgument("9BCB107F-6355-499D-8227-42D68346B67F", $dataSelector);
 	$argument = new Promidata_Service_Request_Exists($existsArgument);
 	$response = $proxy->Exists($argument);
-	
+
     var_dump($response);
-	
+
 
 } catch (Exception $e) {
 
