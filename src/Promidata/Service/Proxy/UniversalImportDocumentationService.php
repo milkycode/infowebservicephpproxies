90af2bcd9653f352dcbfe63391d07db1da55dc11
Chris <christian@milkycode.com>
2016-07-04 16:09:54 +0200

    change copyright

--- src/Promidata/Service/Proxy/UniversalImportDocumentationService.php
+++ src/Promidata/Service/Proxy/UniversalImportDocumentationService.php
@@ -4,7 +4,7 @@
  * @author      Christian Hinz <christian@milkycode.com>
  * @category    Milkycode
  * @package     Promidata_Service
- * @copyright   Copyright (c) 2015 milkycode UG (http://www.milkycode.com)
+ * @copyright   Copyright (c) 2016 milkycode UG (http://www.milkycode.com)
  */
 class Promidata_Service_Proxy_UniversalImportDocumentationService extends Promidata_Service_Proxy_AbstractService implements Promidata_Service_Interface_IUniversalImportDocumentationService
 {
--- src/Promidata/Service/Request/GetArticleNumberListByArticleGroup.php
+++ src/Promidata/Service/Request/GetArticleNumberListByArticleGroup.php
@@ -2,9 +2,10 @@
 /**
  * Proxy for Promidata PromotionalOffice.
  * @author      Christian Hinz <christian@milkycode.com>
+ * @author      Matthias Kleine <matthias@code-x.de>
  * @category    Milkycode
  * @package     Promidata_Service
- * @copyright   Copyright (c) 2015 milkycode UG (http://www.milkycode.com)
+ * @copyright   Copyright (c) 2016 milkycode UG (http://www.milkycode.com)
  */
 class Promidata_Service_Request_GetArticleNumberListByArticleGroup
 {
