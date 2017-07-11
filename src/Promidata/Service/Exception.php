41744a48ef1b213e9b9dce71a8ac4670c34ae0c8
Chris <christian@milkycode.com>
2017-07-11 16:45:32 +0200

    Added getInnerExceptionMessages() to receive inner soap exceptions

--- src/Promidata/Service/Exception.php
+++ src/Promidata/Service/Exception.php
@@ -9,8 +9,34 @@
  */
 class Promidata_Service_Exception extends Exception
 {
+    protected $innerMessages = array();
+
     public function __construct($message = '', $code = 0, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * Get inner soap exceptions (recursive).
+     * @return array
+     */
+    public function getInnerExceptionMessages()
+    {
+        $previous = $this->getPrevious();
+        if (!empty($previous->detail->ExceptionDetail->InnerException)) {
+            $this->getInnerException($previous->detail->ExceptionDetail->InnerException);
+        }
+
+        return $this->innerMessages;
+    }
+
+    protected function getInnerException($e)
+    {
+        if (!empty($e->Message)) {
+            $this->innerMessages[] = $e->Message;
+        }
+        if (!empty($e->InnerException)) {
+            $this->getInnerException($e->InnerException);
+        }
     }
 }
